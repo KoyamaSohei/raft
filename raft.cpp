c63615fb1cf0997d5adb8d39a95428f959abd509
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-08 17:16:39 +0900

    add state

--- raft.cpp
+++ raft.cpp
@@ -3,14 +3,49 @@
 
 namespace tl = thallium;
 
+enum class State {
+  Follower,
+  Candidate,
+  Leader,
+};
+
 class RaftProvider : public tl::provider<RaftProvider> {
+private:
+  // 現在の状態(Follower/Candidate/Leader)
+  State state;
 public:
   RaftProvider(tl::engine& e,uint16_t provider_id=1)
-  : tl::provider<RaftProvider>(e, provider_id) {
+  : tl::provider<RaftProvider>(e, provider_id),
+    state(State::Follower)
+  {
     get_engine().push_finalize_callback(this,[p=this]() {delete p;});
   }
   ~RaftProvider() {
     get_engine().pop_finalize_callback(this);
+  }
+  void runFollower() {
+    
+  }
+  void runCandidate() {
+
+  }
+  void runLeader() {
+
+  }
+  void run() {
+    while(1) {
+      switch (state) {
+      case State::Follower:
+        runFollower();
+        break;
+      case State::Candidate:
+        runCandidate();
+        break;
+      case State::Leader:
+        runLeader();
+        break;
+      }
+    }
   }
 };
 
