7cc66539e2a042f04222d905c102e093dfd3c184
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-08 18:43:17 +0900

    add follower timeout

--- raft.cpp
+++ raft.cpp
@@ -16,6 +16,9 @@ class RaftProvider : public tl::provider<RaftProvider> {
 private:
   // 現在の状態(Follower/Candidate/Leader)
   State state;
+  tl::mutex mu;
+  tl::condition_variable cond;
+  const int TIMEOUT = 3;
 public:
   RaftProvider(tl::engine& e,uint16_t provider_id=1)
   : tl::provider<RaftProvider>(e, provider_id),
@@ -27,10 +30,24 @@ public:
     get_engine().pop_finalize_callback(this);
   }
   void runFollower() {
-
+    std::cout << "become follower" << std::endl;
+    std::unique_lock<tl::mutex> lock(mu);
+    while(1) {
+      timespec deadline;
+      timespec_get(&deadline,TIME_UTC);
+      deadline.tv_sec += TIMEOUT;
+      bool acquired = cond.wait_until(lock,&deadline);
+      if(!acquired) { // timeout
+        state = State::Candidate;
+        break;
+      }
+    }
   }
   void runCandidate() {
-
+    std::cout << "become candidate" << std::endl;
+    while(1) {
+      
+    }
   }
   void runLeader() {
 
@@ -69,7 +86,7 @@ int main(int argc, char** argv) {
     std::cout << "Signal received " << num << std::endl;
     exit(1);
   });
-
+  provider.run();
   myEngine.wait_for_finalize();
   return 0;
 }
