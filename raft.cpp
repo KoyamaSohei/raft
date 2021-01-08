9a36245f881bd73cb28ca1b4836827edb787f6a9
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-08 17:05:48 +0900

    init RaftProvider

--- raft.cpp
+++ raft.cpp
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <thallium.hpp>
 
-int main() {
+namespace tl = thallium;
+
+class RaftProvider : public tl::provider<RaftProvider> {
+public:
+  RaftProvider(tl::engine& e,uint16_t provider_id=1)
+  : tl::provider<RaftProvider>(e, provider_id) {
+    get_engine().push_finalize_callback(this,[p=this]() {delete p;});
+  }
+  ~RaftProvider() {
+    get_engine().pop_finalize_callback(this);
+  }
+};
+
+int main(int argc, char** argv) {
+  tl::engine myEngine("tcp", THALLIUM_SERVER_MODE);
+  std::cout << "Server running at address " << myEngine.self() << std::endl;
+  RaftProvider provider(myEngine);
+  myEngine.wait_for_finalize();
   return 0;
 }
