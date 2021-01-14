2844a4b9a559f090286045a17bee8f459e90c3cc
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-14 22:23:03 +0900

    reafctor

--- logger.hpp
+++ logger.hpp
@@ -24,7 +24,7 @@ public:
   void bootstrap_state_from_log(int &current_term,std::string &voted_for);
   void save_current_term(int current_term);
   void save_voted_for(std::string voted_for);
-  void save_log(int index,std::string log_str);
+  void save_log(int index,std::string json_str);
   std::string get_log(int index);
 };
 
