7a6d6649492847a050e88b1344b2ea8d4025f24f
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-21 23:29:56 +0900

    add GET_HIGHER_TERM

--- test/provider.cpp
+++ test/provider.cpp
@@ -5,6 +5,8 @@
 #include <thallium.hpp>
 
 #define ADDR "127.0.0.1:30000"
+#define CADDR "127.0.0.1:30003"
+
 namespace {
 class provider_test : public ::testing::Test {
 protected:
@@ -12,12 +14,16 @@ protected:
   tl::engine client_engine;
   raft_provider provider;
   tl::remote_procedure m_echo_state_rpc;
+  tl::remote_procedure m_request_vote_rpc;
+  tl::remote_procedure m_append_entries_rpc;
   tl::provider_handle server_addr;
   provider_test()
     : server_engine("sockets://" ADDR, THALLIUM_SERVER_MODE, true, 2)
-    , client_engine("sockets", THALLIUM_CLIENT_MODE)
+    , client_engine("sockets://" CADDR, THALLIUM_CLIENT_MODE)
     , provider(server_engine, RAFT_PROVIDER_ID)
     , m_echo_state_rpc(client_engine.define(ECHO_STATE_RPC_NAME))
+    , m_request_vote_rpc(client_engine.define("request_vote"))
+    , m_append_entries_rpc(client_engine.define("append_entries"))
     , server_addr(tl::provider_handle(client_engine.lookup("sockets://" ADDR),
                                       RAFT_PROVIDER_ID)) {
     server_engine.enable_remote_shutdown();
@@ -54,6 +60,16 @@ protected:
     int r = m_echo_state_rpc.on(server_addr)();
     return raft_state(r);
   }
+
+  append_entries_response append_entries(int term, int prev_index,
+                                         int prev_term,
+                                         std::vector<raft_entry> entries,
+                                         int leader_commit,
+                                         std::string leader_id) {
+    append_entries_response resp = m_append_entries_rpc.on(server_addr)(
+      term, prev_index, prev_term, entries, leader_commit, leader_id);
+    return resp;
+  }
 };
 
 TEST_F(provider_test, BECOME_FOLLOWER) {
@@ -68,6 +84,19 @@ TEST_F(provider_test, BECOME_LEADER) {
   usleep(3 * INTERVAL);
   provider.run();
   ASSERT_EQ(fetch_state(), raft_state::leader);
+}
+
+TEST_F(provider_test, GET_HIGHER_TERM) {
+  std::vector<std::string> nodes;
+  provider.start(nodes);
+  usleep(3 * INTERVAL);
+  provider.run();
+  ASSERT_EQ(fetch_state(), raft_state::leader);
+  append_entries_response r =
+    append_entries(2, 0, 0, std::vector<raft_entry>(), 0, "sockets://" CADDR);
+  ASSERT_TRUE(r.is_success());
+  ASSERT_EQ(r.get_term(), 2);
+  ASSERT_EQ(fetch_state(), raft_state::follower);
 }
 
 } // namespace
