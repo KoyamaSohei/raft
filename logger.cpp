06ffaf71d30a5162c60e3c65a76ba41b7a0baaa1
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-24 23:19:19 +0900

    fix SPECIAL_ENTRY_KEY

--- logger.cpp
+++ logger.cpp
@@ -53,7 +53,7 @@ void lmdb_raft_logger::init(std::string addrs) {
     // index:0 ,term: 0
     stored_log_num = 0;
     std::string uuid, command, log;
-    build_command(command, "SPECIAL_ENTRY_KEY", addrs);
+    build_command(command, SPECIAL_ENTRY_KEY, addrs);
     generate_uuid(uuid);
     build_log(log, 0, uuid, command);
 
