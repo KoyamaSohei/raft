85f4f00514011585b08b7fd54614fe0a71035dea
KoyamaSohei <koyamaso0309@gmail.com>
2021-01-26 22:34:42 +0900

    fix test

--- provider_test.cpp
+++ provider_test.cpp
@@ -579,7 +579,8 @@ TEST_F(provider_test, CLIENT_PUT_LEADER_NOT_FOUND) {
   ASSERT_EQ(r.get_index(), 0);
   usleep(3 * INTERVAL);
   EXPECT_CALL(*logger, set_voted_for_self());
-  EXPECT_CALL(*logger, set_current_term(::testing::Ge(2)));
+  EXPECT_CALL(*logger, set_current_term(::testing::Ge(2)))
+    .Times(::testing::AnyNumber());
   provider->run();
   ASSERT_EQ(fetch_state(), raft_state::candidate);
   client_request_response r2 =
